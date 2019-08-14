650d837d7348b36e62478aa1d18e10b1cbf7815b
maiada <maiadacx@gmail.com>
2019-08-13 20:41:26 -0400

    the wins, losses and guess show up in the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,73 +2,56 @@
 var wins = 0;
 var losses = 0;
 
-// function starts game
-function playGame() {
+// pressing a key starts the game
+document.onkeyup = function playGame() {
 
+    //the pressed key = guess: is stored in variable & shows up on screen
+    var userGuess = event.key;
+    
+    document.getElementById("userinput").innerHTML = userGuess;
+    
+    //computer generates random # 0-25; number gets converted to string a-z
+    var randomNumber = Math.floor(Math.random() * 25);
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var randomLetter = alphabet.charAt(randomNumber);
 
-//reset user guess
-var userGuess = "";
+    // user types a letter and it creates text on page
+    //var userInput = prompt('guess a letter');
+    // this letter is saved as a variable and saved as lower case
+    //gets pushed into an array
+    // the letters are pushed to dom
 
-//reset guesses left
-var guessesLeft =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // there is a for loop that loops through 9 times OR until correct guess
+    // each time it loops through, the number of guesses left decreases
 
-// generate random number
-var randomNumber = Math.floor(Math.random() * 25);
-
-// computer generates # 0-25 number gets converted to string a-z 
-
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-var randomLetter = alphabet.charAt(randomNumber);
-
-// user types a letter and it creates text on page
-var userInput = prompt('guess a letter');
-
-// this letter is saved as a variable and saved as lower case
-
-var userGuess = userInput.toLowerCase();
-
-document.getElementById("userinput").innerHTML = userGuess;
-
-// there is a for loop that loops through 9 times OR until correct guess
-// each time it loops through, the number of guesses left decreases
-
-// if user guess === computer letter then add 1 to wins
-if (userGuess === randomLetter) {
-    wins++
-} 
-
-// if user guess is wrong, add 1 to losses
-else {
-    losses++
-}
-
-// the loop ends and the game resets 
-
-// for test run - end loop and ask if you want to play again
-    var keepPlaying = confirm('want to keep playing?');
-
-    if (keepPlaying === true) {
-        playGame();
+    // if user guess === computer letter then add 1 to wins
+    if (userGuess === randomLetter) {
+        wins++
     }
-//guesses reset
-// guesses left resets
-//wins and losses do NOT reset
 
+    // if user guess is wrong, add 1 to losses
+    else {
+        losses++
+    }
 
-console.log('random number ' + randomNumber);
-console.log('alphabet: ' + alphabet);
-console.log('random letter: ' + randomLetter);
-console.log('the user guesses: ' + userGuess);
-console.log('wins: ' + wins);
-console.log('losses: ' + losses);
-//console.log('guesses left: ' + guessesLeft);
-console.log('keep playing: ' + keepPlaying);
-}
-
-playGame();
-
-if (playGame) {
     document.getElementById('win').textContent = wins;
     document.getElementById('loss').textContent = losses;
+    // the loop ends and the game resets 
+
+    // for test run - end loop and ask if you want to play again
+    //var keepPlaying = confirm('want to keep playing?');
+    //guesses reset
+    // guesses left resets
+    //wins and losses do NOT reset
+
+
+    console.log('random number ' + randomNumber);
+    console.log('alphabet: ' + alphabet);
+    console.log('random letter: ' + randomLetter);
+    console.log('the user guesses: ' + userGuess);
+    console.log('wins: ' + wins);
+    console.log('losses: ' + losses);
+    //console.log('guesses left: ' + guessesLeft);
+    
 }
+
